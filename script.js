b81c26b58c133d0c263ed133fef2dacf64141d7a
example <alisayar0107@gmail.com>
2021-02-12 16:41:36 +0300

    asynchronous function added for fetching api, and passing it to an array

--- script.js
+++ script.js
@@ -2,5 +2,23 @@
 
 // NASA API KEY
 const count = 10;
-const apiKey = ``
+const apiKey = ``;
 const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
+
+let resultsArray = [];
+
+// Get 10 images from API
+async function getNasaPictures() {
+    try {
+     const response = await fetch(apiUrl)
+     resultsArray = await response.json();
+     console.log(resultsArray);
+    } catch(err) {
+     // Catch if there is an error
+     console.log(err);
+    }
+}
+
+
+// On load.
+getNasaPictures(); 
