b46ac8a6a5785f40950125053d308db83caf6491
example <alisayar0107@gmail.com>
2021-02-14 13:05:00 +0300

    we splitted updateDOM function to another function called createDOMNodes

--- script.js
+++ script.js
@@ -12,60 +12,63 @@ const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${co
 let resultsArray = [];
 let favorites = {};
 
+function createDOMNodes() {
+  resultsArray.forEach((result) => {
+    // Card container element
+    const card = document.createElement('div');
+    card.classList.add('card');
+    // Link for url to view hd image on NASA.
+    const link = document.createElement('a');
+    link.href = result.hdurl;
+    link.title = 'View Full Image on NASA';
+    link.target = '_blank';
+    // Image url for dynamically show it.
+    const image = document.createElement('img');
+    image.src = result.url;
+    image.alt = "NASA Astronomy Picture of the Day";
+    image.loading = 'lazy';
+    image.classList.add('card-img-top');
+
+    // Card body elements
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+    const cardTitle = document.createElement('h5');
+    // Card Title Element
+    cardTitle.classList.add('card-title');
+    cardTitle.textContent = result.title;
+    // Paragraph elements 
+    // -SaveText-
+    const saveText = document.createElement('p');
+    saveText.classList.add('clickable');
+    saveText.textContent = 'Add it to the your Favourites!';
+    saveText.setAttribute('onclick', `saveFavorite('${result.url}')`);
+    // -CardText-
+    const cardText = document.createElement('p');
+    cardText.classList.add('card-text');
+    cardText.textContent = result.explanation;
+
+    // Footer container
+    const footer = document.createElement('small');
+    footer.classList.add('text-muted');
+    // Date 
+    const date = document.createElement('strong');
+    date.textContent = result.date;
+    // Copyright
+    const copyrightResult = result.copyright === undefined ? '' : result.copyright;
+    const copyright = document.createElement('span');
+    copyright.textContent = ` ${copyrightResult}`;
+
+    // Appending elements
+    footer.append(date, copyright);
+    cardBody.append(cardTitle, saveText, cardText, footer);
+    link.appendChild(image);
+    card.append(link, cardBody);
+    imagesContainer.appendChild(card);
+ });
+}
 
 function updateDOM() {
-  resultsArray.forEach((result) => {
-     // Card container element
-     const card = document.createElement('div');
-     card.classList.add('card');
-     // Link for url to view hd image on NASA.
-     const link = document.createElement('a');
-     link.href = result.hdurl;
-     link.title = 'View Full Image on NASA';
-     link.target = '_blank';
-     // Image url for dynamically show it.
-     const image = document.createElement('img');
-     image.src = result.url;
-     image.alt = "NASA Astronomy Picture of the Day";
-     image.loading = 'lazy';
-     image.classList.add('card-img-top');
-
-     // Card body elements
-     const cardBody = document.createElement('div');
-     cardBody.classList.add('card-body');
-     const cardTitle = document.createElement('h5');
-     // Card Title Element
-     cardTitle.classList.add('card-title');
-     cardTitle.textContent = result.title;
-     // Paragraph elements 
-     // -SaveText-
-     const saveText = document.createElement('p');
-     saveText.classList.add('clickable');
-     saveText.textContent = 'Add it to the your Favourites!';
-     saveText.setAttribute('onclick', `saveFavorite('${result.url}')`);
-     // -CardText-
-     const cardText = document.createElement('p');
-     cardText.classList.add('card-text');
-     cardText.textContent = result.explanation;
-
-     // Footer container
-     const footer = document.createElement('small');
-     footer.classList.add('text-muted');
-     // Date 
-     const date = document.createElement('strong');
-     date.textContent = result.date;
-     // Copyright
-     const copyrightResult = result.copyright === undefined ? '' : result.copyright;
-     const copyright = document.createElement('span');
-     copyright.textContent = ` ${copyrightResult}`;
-
-     // Appending elements
-     footer.append(date, copyright);
-     cardBody.append(cardTitle, saveText, cardText, footer);
-     link.appendChild(image);
-     card.append(link, cardBody);
-     imagesContainer.appendChild(card);
-  });
+  createDOMNodes();
 }
 
 // Get 10 images from API
